09caa5256d5f12d3b06f0874b685b27565e3a252
DESKTOP-GO74Q4B\tyhug <tyhugtfrExtra@gmail.com>
2025-01-06 09:22:48 +0100

    updated the split in training

--- TrainingModelModule.py
+++ TrainingModelModule.py
@@ -11,7 +11,7 @@ X = data.drop(columns=['target'])
 y = data['target']
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Initialize the Random Forest model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
